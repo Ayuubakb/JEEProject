9682bbfd1ed2a12e21876de4fd7c9f594084ba99
Ayoub-Akb <ayoubakoubri@gmail.com>
2024-10-28 15:19:52 +0100

    [BACKEND] springSecurity

--- Backend/Backend/src/main/java/com/JEEproject/Backend/configs/SecurityConfiguration.java
+++ Backend/Backend/src/main/java/com/JEEproject/Backend/configs/SecurityConfiguration.java
@@ -1,13 +1,12 @@
-package com.JEEproject.Backend.config;
+package com.JEEproject.Backend.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
